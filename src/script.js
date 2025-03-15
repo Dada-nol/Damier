7fd0c872b2de4fcd91b2f3d25e20bf6254de076d
Darren <nardol.darren@gmail.com>
2025-03-15 15:34:25 +0100

    ADD board and style

--- src/script.js
+++ src/script.js
@@ -1,18 +1,30 @@
 // Création d'un damier en HTML et CSS
 
 const body = document.querySelector("body");
-body.style.textAlign = "center";
+body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.alignItems = "center";
 
 const title = document.createElement("h1");
 title.textContent = "Damier";
 body.appendChild(title);
 
+const container = document.createElement("div");
+container.style.display = "flex";
+container.style.flexDirection = "column";
+container.style.width = "500px";
+container.style.height = "500px";
+body.appendChild(container);
+
 // Création des colonnes
 
 const columns = document.createElement("div");
+columns.style.height = "20px";
+columns.style.marginLeft = "20px";
 columns.style.display = "flex";
 columns.style.justifyContent = "center";
-columns.style.gap = "1rem";
+columns.style.alignItems = "center";
+columns.style.gap = "50px";
 
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
@@ -27,9 +39,12 @@ for (let i = 0; i < letters.length; i++) {
 // Création des lignes
 
 const rows = document.createElement("div");
+rows.style.width = "20px";
 rows.style.display = "flex";
 rows.style.flexDirection = "column";
-rows.style.gap = "1rem";
+rows.style.justifyContent = "center";
+rows.style.alignItems = "center";
+rows.style.gap = "40px";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -41,13 +56,20 @@ for (let i = 0; i < numbers.length; i++) {
   rows.appendChild(row);
 }
 
-body.appendChild(columns);
-body.appendChild(rows);
-
-// Test de la sélection de la première colonne
-const test = document.querySelector(".column-A");
-test.style.color = "red";
-
 // Création du damier
 
 const board = document.createElement("div");
+board.style.display = "flex";
+board.style.flex = "1";
+board.style.flexWrap = "wrap";
+board.style.backgroundColor = "black";
+
+const baseline = document.createElement("div");
+baseline.style.display = "flex";
+baseline.style.height = "100%";
+
+baseline.appendChild(rows);
+baseline.appendChild(board);
+
+container.appendChild(columns);
+container.appendChild(baseline);
